1bf38ab43ca2e8650fc3e03beec4ccf7d562499f
xVjVx <jaimedsm127@gmail.com>
2022-03-30 13:04:03 -0400

    Update code

--- array_calc.c
+++ array_calc.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(){
+bool ascending(double a, double b){
+    return a > b;
+}
+
+bool descending(double a, double b){
+    return a < b;
+}
+
+int main(int argc, char** argv){
     char buffer[16];
     double array[10];
     int counter = 0;
+    int counter2 = 0;
     double auxiliary;
 
     double max = -DBL_MAX;
@@ -17,8 +28,17 @@ int main(){
 
     char positives = 0;
     char negatives = 0;
+    char msg[12];
+    
+    bool (*predicate)(double, double);
 
-    int numbers = 0;
+    if (argc > 1 && strcmp(argv[1], "descending") == 0) {
+        predicate = descending;
+        sprintf(msg, "descending");
+    }else{
+        predicate = ascending;
+        sprintf(msg, "ascending");
+    }
 
     for (counter = 0; counter < 10; counter++) {
         do{
@@ -40,22 +60,22 @@ int main(){
         }
     }
     for (counter = 0; counter < 10; counter++){
-        for (numbers = counter + 1; numbers < 10; numbers++){
-            if (array[counter] > array[numbers]){
+        for (counter2 = counter + 1; counter2 < 10; counter2++){
+            if (predicate(array[counter], array[counter2])){
                 auxiliary = array[counter];
-                array[counter] = array[numbers];
-                array[numbers] = auxiliary;
+                array[counter] = array[counter2];
+                array[counter2] = auxiliary;
             }
         }
     }
-    printf("The numbers in ascending order: ");
+    printf("The numbers in %s order: ", msg);
     for (counter = 0; counter < 10; counter++){
         printf("%lg%s", array[counter], counter < 9 ? ", " : "\n");
         sum += array[counter];
         product *= array[counter];
     }
     average = sum /10;
-    printf("Exist %hhd numbers negatives and %hhd numbers positives\n", negatives, positives);
+    printf("There are %hhd negative numbers and %hhd positive numbers\n", negatives, positives);
     printf("Max: %lg\nMin: %lg\n", max, min);
     printf("The average is: %lg\n", average);
     printf("The sum of values is: %lg\n", sum);
